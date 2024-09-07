e0df7e2f411e307a2949029352f6560cc9c40f65
Vitor Morita <vmmorita@myseneca.ca>
2024-09-07 02:51:48 -0400

    Individual message or note.

---
+++ lab2a.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+#Author: Vitor
+# Author ID: vmmorita@myseneca.ca
+# Date Created: 2024/09/07
+
+name = 'Jon'
+age = 20
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Vitor Mendes Morita
+#Author ID: vmmorita@myseneca.ca
+#Date Created: 2024/9/4
+import sys
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+
+print('blast off!')
